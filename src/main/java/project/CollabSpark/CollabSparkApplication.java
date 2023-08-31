dade6aeafaadc634e8c0d784dda49716e386063f
Özge Odabaş <180315051@ogr.cbu.edu.tr>
2023-08-31 23:37:00 +0300

    Project creation

---
+++ src/main/java/project/CollabSpark/CollabSparkApplication.java
@@ -0,0 +1,13 @@
+package project.CollabSpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabSparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollabSparkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/CollabSpark/CollabSparkApplicationTests.java
@@ -0,0 +1,13 @@
+package project.CollabSpark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabSparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
