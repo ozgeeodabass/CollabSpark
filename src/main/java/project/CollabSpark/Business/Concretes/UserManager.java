679cb8d0d35a965b34a4d2d77411b8b702157604
Özge Odabaş <180315051@ogr.cbu.edu.tr>
2023-09-01 13:37:49 +0300

    User Controller class created. constructor injection used to reduce dependencies among classes

--- src/main/java/project/CollabSpark/Business/Concretes/UserManager.java
+++ src/main/java/project/CollabSpark/Business/Concretes/UserManager.java
@@ -1,9 +1,11 @@
 package project.CollabSpark.Business.Concretes;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import project.CollabSpark.Business.Abstracts.UserService;
 import project.CollabSpark.DataAccess.Abstracts.UserRepository;
 
+@Service
 public class UserManager implements UserService {
 
     private UserRepository userRepository;
---
+++ src/main/java/project/CollabSpark/Controllers/UserController.java
@@ -0,0 +1,19 @@
+package project.CollabSpark.Controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import project.CollabSpark.Business.Abstracts.UserService;
+
+@RestController
+@RequestMapping(name = "/users")
+public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+}
